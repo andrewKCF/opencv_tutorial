20cd0506c908bff648870287c463dccf4d361a4f
andrewKCF <104657575+andrewKCF@users.noreply.github.com>
2024-11-01 11:00:29 +0800

    Add files via upload

--- pra/07_face_camera/face_camera_pra.py
+++ pra/07_face_camera/face_camera_pra.py
@@ -3,3 +3,20 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 '''
+import cv2
+
+# Load the cascade
+#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0) 
+
+while True: 
+    ret, img = cap.read()
+
+    cv2.imshow("Video", img) 
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break;
+
+cap.release()
+cv2.destroyAllWindows()
